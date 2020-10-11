4e97766737fb51b88a84d273de5695ebc5041ba3
Samar Kalra <47415857+samarcodes@users.noreply.github.com>
2020-10-11 22:52:04 +0530

    Create adjacency_list.cpp

---
+++ list/adjacency_list.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<list>
+
+using namespace std;
+
+int main() {
+	list<pair<int, int>> *l;
+	int n;
+	cin >> n;
+
+	l = new list<pair<int, int>>[n];
+
+	int edges;
+	cin >> edges;
+	for (int i = 0; i < edges; i++) {
+		int x, y, wt;
+		cin >> x >> y >> wt;
+
+		l[x].push_back(make_pair(y, wt));
+		l[y].push_back(make_pair(x, wt));
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (auto xp : l[i]) {
+			cout << "(" << xp.first << "," << xp.second << ")";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
